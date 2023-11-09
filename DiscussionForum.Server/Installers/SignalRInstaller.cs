45c4fd81f2fbd5c1c8aa3641eb6ee24cd64be273
Markus Nisula <markus.nisula@solita.fi>
2023-11-09 13:32:32 +0200

    Fix services to use prod values

--- DiscussionForum.Server/Installers/SignalRInstaller.cs
+++ DiscussionForum.Server/Installers/SignalRInstaller.cs
@@ -1,20 +1,16 @@
-﻿using MessagePack;
-using Microsoft.AspNetCore.ResponseCompression;
-
-namespace DiscussionForum.Server.Installers;
+﻿namespace DiscussionForum.Server.Installers;
 
 public class SignalRInstaller : IInstaller
 {
     public void Install(WebApplicationBuilder builder)
     {
-        //if (builder.Environment.IsDevelopment())
-        //{
-            builder.Services.AddSignalR(o => o.EnableDetailedErrors = true)
-                .AddMessagePackProtocol();
-        //}
-        //else
-        //{
-        //    builder.Services.AddSignalR().AddMessagePackProtocol().AddAzureSignalR();
-        //}
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddSignalR(o => o.EnableDetailedErrors = true).AddMessagePackProtocol();
+        }
+        else
+        {
+            builder.Services.AddSignalR().AddMessagePackProtocol().AddAzureSignalR();
+        }
     }
 }
