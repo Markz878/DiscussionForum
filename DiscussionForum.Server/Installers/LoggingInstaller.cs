43d01bd9aa2649cb6ac6f442394ac76e102f0f76
Markus Nisula <markus.nisula@solita.fi>
2023-11-10 15:36:52 +0200

    Add more request process ignores

--- DiscussionForum.Server/Installers/LoggingInstaller.cs
+++ DiscussionForum.Server/Installers/LoggingInstaller.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
+using System.Linq;
 
 namespace DiscussionForum.Server.Installers;
 
@@ -19,6 +20,7 @@ public class LoggingInstaller : IInstaller
     }
 }
 
+
 public class IgnoreRequestPathsTelemetryProcessor : ITelemetryProcessor
 {
     private readonly ITelemetryProcessor _next;
@@ -27,16 +29,35 @@ public class IgnoreRequestPathsTelemetryProcessor : ITelemetryProcessor
     {
         _next = next;
     }
+
     public void Process(ITelemetry telemetry)
     {
         if (telemetry is RequestTelemetry requestTelemetry)
         {
             // Check if the request path is "/health"
-            if (requestTelemetry.Url.AbsolutePath.Equals("/health", StringComparison.OrdinalIgnoreCase))
+            if (SkipTelemetry(requestTelemetry.Url.AbsolutePath))
             {
                 return;
             }
         }
         _next.Process(telemetry);
+    }
+
+    private static readonly string[] _ignorePaths = ["/health", "/favicon.ico", "/topichub/negotiate"];
+    private static readonly string[] _fileEndings = [".br", ".js", ".svg", ".png", ".css"];
+    private static bool SkipTelemetry(string path)
+    {
+        if (_ignorePaths.Contains(path))
+        {
+            return true;
+        }
+        foreach (string fileEnding in _fileEndings)
+        {
+            if (path.EndsWith(fileEnding))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
