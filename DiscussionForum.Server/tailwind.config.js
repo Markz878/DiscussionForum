b65ce25f6316caac048c490a20b0f88ee4456468
Markus Nisula <markus.nisula@solita.fi>
2023-11-21 15:49:08 +0200

    Fix styles

--- DiscussionForum.Server/tailwind.config.js
+++ DiscussionForum.Server/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './Components/**/*.razor',
+        './Pages/**/*.razor',
         '../DiscussionForum.Client/Components/**/*.razor'
     ],
     theme: {
